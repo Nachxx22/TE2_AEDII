5b9bcd0f20b8998b9e5e4a636f687ae1c7a80fc0
Nacho <Nacho@DESKTOP-0H8MH7V>
2023-03-26 11:30:11 -0600

    Agregar archivos de proyecto.

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnButtonClicked(object sender, EventArgs args)
+        {
+            string text1 = entry1.Text;
+            string text2 = entry2.Text;
+            string result = $"{text1}{text2}";
+
+            labelResult.Text = result;
+        }
+    }
+}
